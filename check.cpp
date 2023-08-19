df105037bf429bcb8ba62a1755127299f6f50099
Anonna <meftahul@iut-dhaka.edu>
2023-08-19 20:58:05 +0600

    2nd commit

--- check.cpp
+++ check.cpp
@@ -1,6 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+class Node
+{
+    public :
+    int data;
+    Node *next;
+    // constructor declare
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
 int main()
 {
-    cout<<"checking";
+    // cout<<"checking";
+    Node * node1=new Node(50); //dynamic declaration
+cout<<node1 -> data<<endl;
+cout<<node1->next <<endl;
+
 }
